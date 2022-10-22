30051621a55a31ac958bc10418355a6e6d859ac3
Mikhail Kornilov <korn-m@yandex-team.ru>
2022-10-22 17:07:58 +0300

    Setup

---
+++ file_manager/__init__.py
@@ -0,0 +1,19 @@
+"""
+Backend часть для сервиса аналогичного Yandex Disk, вступительное испытание
+в академию.
+"""
+__author__ = 'Kornilov Mikhail'
+__maintainer__ = __author__
+
+__email__ = 'm24122002@gmail.com'
+__license__ = 'MIT'
+__version__ = '0.0.1'
+
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__',
+)
